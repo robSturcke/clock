7fe94640c40132aea87cd858ce8199de31d6408f
Rob Sturcke <rs@rsvision.com>
2017-02-17 00:09:06 -0500

    still need webkit CSS however working on Chrome

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,22 @@
 const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.mins-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 
 function setDate() {
     const now = new Date();
     const seconds = now. getSeconds();
-    const secondDegrees = (seconds / 60) * 360
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = 'rotate(${secondDegrees}deg)';
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = 'rotate(${minsDegrees}deg)';
+
+    const hour = now.getMinutes();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = 'rotate(${hourDegrees}deg)';
+
 };
 
 setInterval(setDate, 1000);
